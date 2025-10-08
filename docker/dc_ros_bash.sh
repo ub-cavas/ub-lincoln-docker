fd58447d7b5eb759a2ec7445aa45a4a514706a21
harsha <hmeka@buffalo.edu>
2025-10-08 01:56:38 -0400

    remove unused files

--- docker/dc_ros_bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-service="ros"
-if [ -n "$1" ]; then
-    service="$1"
-fi
-
-docker compose exec -it $service /bin/bash
--- docker/dc_ros_up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service="ros"
-if [ -n "$1" ]; then
-    service="$1"
-fi
-
-../scripts/host_config_dds.bash
-docker compose up -d $service
