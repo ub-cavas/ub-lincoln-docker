1a3f4f513957513db1afd520ad05d53ab338a7a5
smk32 <1235477+smk32@users.noreply.github.com>
2025-10-14 20:51:36 -0400

    Quick fixes for image build 20251014.0

--- docker/resources/download_maps.sh
+++ docker/resources/download_maps.sh
@@ -5,7 +5,7 @@ wget -O accel_brake_map.tar.xz https://buffalo.box.com/shared/static/zytwnlemxjt
 tar -xvf accel_brake_map.tar.xz
 rm accel_brake_map.tar.xz
 
-# Download Map & its components
+# Download HD map & its components
 wget -O ub_hdmap.tar.xz https://buffalo.box.com/shared/static/ieqo2qw17kucgkqdg3lejhrkhpx7fimx.xz
 tar -xvf ub_hdmap.tar.xz
 rm ub_hdmap.tar.xz
--- docker/resources/set_custom_autoware_params.sh
+++ docker/resources/set_custom_autoware_params.sh
@@ -5,16 +5,16 @@
 # Change Max Velocity to 40 kph (11.12 m/s)
 sed -i 's|max_vel: 4.17 |max_vel: 11.12|' /autoware/src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
 
-# Change Engage Velocity to 1 m/s because 0.25 is not enough to start MKZ moving.
+# Change Engage Velocity to 1 m/s because 0.25 is not enough to start MKZ moving
 sed -i 's|engage_velocity: 0.25|engage_velocity: 1.0 |' /autoware/src/launcher/autoware_launch/autoware_launch/config/planning/scenario_planning/common/autoware_velocity_smoother/velocity_smoother.param.yaml
 
-#Comment out the launch commands for HddMonitor in autoware_system_monitor package.
+# Comment out the launch commands for HddMonitor in autoware_system_monitor package
 sed -i '/<composable_node[^>]*plugin="HddMonitor"/,/<\/composable_node>/ {
   /<composable_node[^>]*plugin="HddMonitor"/ s/^/<!-- /
   /<\/composable_node>/ s/$/ -->/
 }' /autoware/src/universe/autoware_universe/system/autoware_system_monitor/launch/system_monitor.launch.xml
 
-# Move Calibrated Accel & Brake maps into autoware.
+# Move Calibrated Accel & Brake maps into Autoware
 mv /resources/autoware_accel_brake_calibrator_files/accel_map.csv /autoware/src/universe/autoware_universe/vehicle/autoware_raw_vehicle_cmd_converter/data/default/accel_map.csv
 mv /resources/autoware_accel_brake_calibrator_files/brake_map.csv /autoware/src/universe/autoware_universe/vehicle/autoware_raw_vehicle_cmd_converter/data/default/brake_map.csv
 rm -rf /resources/autoware_accel_brake_calibrator_files
