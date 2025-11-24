6806647cb0cfa4aad742411ae5f4814e6f8d7cc3
harsha <hmeka@buffalo.edu>
2025-11-24 18:01:08 -0500

    add slam and qol changes

---
+++ docker/resources/download_assets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download Accel_map & Brake_map
+wget -O accel_brake_map.tar.xz https://buffalo.box.com/shared/static/zytwnlemxjtsecqb5jypjxvv3j6ghbeg.xz
+tar --no-same-owner -xvf accel_brake_map.tar.xz
+rm accel_brake_map.tar.xz
+
+# Download Camera Files
+wget -O camera_files.tar.xz https://buffalo.box.com/shared/static/iwnncf6fw59qo9o3sufho4kelby1l4ze.xz
+tar --no-same-owner -xvf camera_files.tar.xz
+rm camera_files.tar.xz
